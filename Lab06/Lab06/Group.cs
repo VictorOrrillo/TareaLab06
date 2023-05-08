5f260dbd427d1ee121d176a3fc6eeadbc19804c0
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-05-08 00:40:51 -0500

    Agregar archivos de proyecto.

---
+++ Lab06/Lab06/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Lab06
+{
+    public class Group : List<Item>
+    {
+        public string Title { get; set; }
+
+        public Group(string title)
+        {
+            Title = title;
+        }
+    }
+}
---
+++ Lab06/Lab06/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item15.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewGroup());
+            };
+        }
+    }
+}
